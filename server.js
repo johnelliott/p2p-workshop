4b3b49d112a637db98b0e41a1a42477cd52dc72e
John Elliott <johnelliott703@gmail.com>
2016-03-12 14:59:20 -0500

    do 2

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var net = require('net');
+var streamSet = require('stream-set');
+
+var activeSockets = streamSet();
 
 var server = net.createServer((socket)=>{
+
+  activeSockets.add(socket);
+
+  // will print "set size is 1"
+  console.log('set size is', activeSockets.size)
+  socket.on('close', function () {
+    // will print "set size is 0"
+    console.log('set size is', activeSockets.size)
+  })
+
   socket.on('data', (data)=>{
     socket.write(data);
-  });
-  socket.on('end', ()=>{
-    server.close(()=>{
-      console.log('server closed');
-    });
   });
 });
 
