493440bf80c12cc7da78c1337fe03538e7625668
John Elliott <johnelliott703@gmail.com>
2016-03-12 15:31:17 -0500

    do 3 with nicknames

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 var net = require('net');
 var streamSet = require('stream-set');
+var jsonStream = require('duplex-json-stream');
 
 var activeSockets = streamSet();
 
 var server = net.createServer((socket)=>{
 
+  // turn the transport stream into an object stream
+  socket = jsonStream(socket)
   activeSockets.add(socket);
 
-  // will print "set size is 1"
-  console.log('set size is', activeSockets.size)
-  socket.on('close', function () {
-    // will print "set size is 0"
-    console.log('set size is', activeSockets.size)
-  })
+  // will print "clients: 1"
+  console.log('clients:', activeSockets.size)
 
   socket.on('data', (data)=>{
     activeSockets.forEach((s)=>{
-      s.write(data);
+      s.write(`${data.nick} > ${data.msg}`);
     });
   });
+  socket.on('close', function () {
+    // will print "clients: 0"
+    console.log('clients:', activeSockets.size)
+  })
 });
 
 server.listen(8124, ()=>{
